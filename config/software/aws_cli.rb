617c46ad6460eb4777d942086af4f056f8b816b3
ccloes <chad_cloes@intuit.com>
2013-03-06 13:25:22 -0800

    Updated to include omnibus-ruby private repo which includes zip support

--- config/software/aws_cli.rb
+++ config/software/aws_cli.rb
@@ -4,7 +4,9 @@ version "0.7.0"
 dependencies ["rubygems", "ruby", "python", "virtualenv"]
 
 source :url => "https://github.com/aws/aws-cli/archive/develop.zip",
-       :md5 => "0b9d9973636f67b77b036844ca9db544"
+       :md5 => "81311b63be43b02b6c131102356bbf6c"
+
+relative_path "aws-cli-develop"
 
 build do
   command ". #{install_dir}/bin/activate; pip install --install-option='--prefix=#{install_dir}' awscli"
