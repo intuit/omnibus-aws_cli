69d34d1c3686f600fc1aa92ec3b5696e7f8161d4
ccloes <chad_cloes@intuit.com>
2013-04-22 12:49:00 -0700

    Updated to version 0.9.1

--- config/projects/aws_cli.rb
+++ config/projects/aws_cli.rb
@@ -1,7 +1,7 @@
 name            "aws_cli"
 
 install_path    "/opt/aws_cli"
-build_version   "0.8.1"
+build_version   "0.9.1"
 build_iteration "1"
 
 dependencies    ["preparation", "aws_cli"]
--- config/software/aws_cli.rb
+++ config/software/aws_cli.rb
@@ -1,12 +1,12 @@
 name "aws_cli"
-version "0.8.1"
+version "0.9.1"
 
 dependencies ["openssl", "python", "virtualenv"]
 
-source :url => "https://github.com/aws/aws-cli/archive/0.8.1.zip",
-       :md5 => "471010a0ed739cc0d361797d7f773b8c"
+source :url => "https://github.com/aws/aws-cli/archive/0.9.1.zip",
+       :md5 => "f78fe9ebf3344e3aae4b331211c92bd8"
 
-relative_path "aws-cli-0.8.1"
+relative_path "aws-cli-0.9.1"
 
 build do
   command ". #{install_dir}/bin/activate; pip install --install-option='--prefix=#{install_dir}' awscli"
