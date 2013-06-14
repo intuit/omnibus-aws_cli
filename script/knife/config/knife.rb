225ad8f41e69a11f893053d8a09018a646b74a8d
capen <cbrinkley@gmail.com>
2013-06-14 14:51:53 -0700

    setting omnibus.erb to build against

--- script/knife/config/knife.rb
+++ script/knife/config/knife.rb
@@ -1,6 +1,6 @@
 knife[:aws_access_key_id]     = "#{ENV['AWS_ACCESS_KEY_ID']}"
 knife[:aws_secret_access_key] = "#{ENV['AWS_SECRET_ACCESS_KEY']}"
-knife[:aws_ssh_key_id]        = 'jenkins'    
+knife[:aws_ssh_key_id]        = 'jenkins'
 knife[:chef_mode]             = 'solo'
 knife[:flavor]                = 'm1.large'
 knife[:identify_file]         = '~/.ssh/id_rsa'
