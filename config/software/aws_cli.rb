1859f798170b89bb5cde423ccfe4f28e6449b852
capen <cbrinkley@gmail.com>
2013-06-14 13:24:04 -0700

    updating omnibus to 1.1.0

--- config/software/aws_cli.rb
+++ config/software/aws_cli.rb
@@ -9,5 +9,6 @@ source :url => "https://github.com/aws/aws-cli/archive/0.9.1.zip",
 relative_path "aws-cli-0.9.1"
 
 build do
-  command ". #{install_dir}/bin/activate; pip install --install-option='--prefix=#{install_dir}' awscli"
+  command ". #{install_dir}/bin/activate"
+  command "pip install --install-option='--prefix=#{install_dir}' awscli"
 end
