ba05e556d3045b50049fe3782eb8172c573db3a6
capen <cbrinkley@gmail.com>
2013-06-14 23:25:44 -0700

    fixing configuration issues.

--- config/projects/aws_cli.rb
+++ config/projects/aws_cli.rb
@@ -4,8 +4,7 @@ homepage        "http://www.intuit.com"
 
 replaces        "aws_cli"
 install_path    "/opt/aws_cli"
-build_version   Omnibus::BuildVersion.new.semver
-#build_version   "0.9.1"
+build_version   "0.9.1"
 build_iteration "1"
 
 dependencies    ["preparation", "aws_cli", "version-manifest"]
