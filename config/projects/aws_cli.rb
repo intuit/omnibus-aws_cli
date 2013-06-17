3136da2d68ade53d2bb128a1eebdeaf063ad1572
capen <cbrinkley@gmail.com>
2013-06-17 14:44:45 -0700

    fixing build iteration.

--- config/projects/aws_cli.rb
+++ config/projects/aws_cli.rb
@@ -5,7 +5,7 @@ homepage        "http://www.intuit.com"
 replaces        "aws_cli"
 install_path    "/opt/aws_cli"
 build_version   "0.9.1"
-build_iteration "1"
+build_iteration "2"
 
 dependencies    ["preparation", "aws_cli", "version-manifest"]
 
