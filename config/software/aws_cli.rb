4633de8821539bd0a7d8ed21e85a658895a738b8
capen <cbrinkley@gmail.com>
2013-06-18 00:00:07 -0700

    try to install specific version

--- config/software/aws_cli.rb
+++ config/software/aws_cli.rb
@@ -3,11 +3,8 @@ version "0.9.1"
 
 dependencies ["openssl", "python", "virtualenv"]
 
-source :url => "https://github.com/aws/aws-cli/archive/0.9.1.zip",
-       :md5 => "f78fe9ebf3344e3aae4b331211c92bd8"
-
-relative_path "aws-cli-0.9.1"
+relative_path "aws-cli-#{version}"
 
 build do
-  command ". #{install_dir}/bin/activate; pip install --install-option='--prefix=#{install_dir}' awscli"
+  command ". #{install_dir}/bin/activate; pip install --install-option='--prefix=#{install_dir}' awscli==#{version}"
 end
